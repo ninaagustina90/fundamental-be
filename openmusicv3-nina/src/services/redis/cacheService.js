f3240240c98f1f5f306e4e07d4c8580bc8b2dd94
Nina Agustina <ninaagustina015@gmail.com>
2025-07-30 20:42:44 +0700

    cache service redis

--- openmusicv3-nina/src/services/redis/cacheService.js
+++ openmusicv3-nina/src/services/redis/cacheService.js
@@ -5,41 +5,28 @@ class CacheService {
     this._client = redis.createClient({
       socket: {
         host: process.env.REDIS_SERVER,
-        port: Number(process.env.REDIS_PORT), 
       },
     });
 
-    this._client.on('connect', () => {
-      console.log('✅ Redis connected on', process.env.REDIS_SERVER + ':' + process.env.REDIS_PORT);
-    });
-
     this._client.on('error', (error) => {
-      console.error('❌ Redis error:', error.message);
+      console.error(error);
     });
 
-    this._client.connect().catch((err) => {
-      console.error('Redis connection error:', err.message);
-    });
+    this._client.connect();
   }
 
- 
-  set(key, value) {
-    const EXPIRATION = 1800; 
-    return this._client.set(key, value, {
-      EX: EXPIRATION,
+  async set(key, value, expirationInSecond = 1800) {
+    await this._client.set(key, value, {
+      EX: expirationInSecond,
     });
   }
-
 
   async get(key) {
     const result = await this._client.get(key);
-    if (result === null) {
-      throw new Error('Cache tidak ditemukan');
-    }
-    return result.toString();
+    if (result === null) throw new Error('Cache tidak ditemukan');
+    return result;
   }
 
- 
   delete(key) {
     return this._client.del(key);
   }
